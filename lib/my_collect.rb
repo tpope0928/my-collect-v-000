026f42b229304a80ee8f308faf23463662bf15f2
Thomas Pope <tpope0928@gmail.com>
2018-08-18 00:02:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,4 @@
-def my_collect
-  i = 0
-  collection = []
-  while i < array.count
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-  end
-end
+
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
